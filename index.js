9ef28e909ce710af504a284e195f210d96ef9295
Pierre Besson <pierre.besson7@gmail.com>
2015-09-11 10:56:51 +0200

    [first version]

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ var pick = require('lodash');
 var dir = require('node-dir');
 var assign = require('object-assign');
 var fs = require('fs');
+var toCheck = [/\.common\.button\.action/, /button/, /\.common\.icon/, /icon/];
 var errors = {};
-dir.readFiles(proccess.argv[0],
-    {match: /.jsx/},
-    function eachFileCb(err, content, filename, next) {
-        if (err) {throw err;}
-    },
-    function finishCb(err, files){
-        if (err) {throw err;}
-            //console.log('finished reading files:', files);
-            // console.log('components', components);
-        _createComponentJSON(components, './src/showcase/catalog/components.json')
-});
-
-function _createComponentJSON(data, outputFilename){
+function _createMigrationReport(data, outputFilename){
     fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
         if(err) {
           console.log(err);
         } else {
-          console.log("Components.json saved to " + outputFilename);
+          console.log("migration-report.json saved to " + outputFilename);
         }
     });
 }
+dir.readFiles(process.argv[2],
+    {match: /\.(jsx|js)$/},
+    function eachFileCb(err, content, filename, next) {
+
+        errors[filename] = [];
+        if (err) {throw err;}
+        toCheck.forEach(function(toCh){
+var matches =content.match(toCh);
+            if(matches !== null){
+                errors[filename].push(matches);
+            }
+        });
+        if(errors[filename].length === 0){
+            delete errors[filename];
+        }
+        next();
+    },
+    function finishCb(err, files){
+        if (err) {throw err;}
+            console.log('finished reading files:', files);
+            // console.log('components', components);
+        _createMigrationReport(errors, './migration-report.json')
+});
